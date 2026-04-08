6b97a68308169a6686b377c4c5cdfd5e54273c7d
Kavitha04-04Sonachalam <kavithaannakkili@gmail.com>
2026-04-08 13:06:05 +0530

    access changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://3.110.32.73:8000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.indinexz.com';
