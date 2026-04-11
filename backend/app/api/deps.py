1d289e9c6b48ebab35fa17d6bac8d10ead60adcf
Kavitha04-04Sonachalam <kavithaannakkili@gmail.com>
2026-04-11 11:10:05 +0530

    swagger ui changes

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,14 +1,18 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from ..core.database import get_db
 from ..core.config import settings
 from ..models.user import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/login")
+security = HTTPBearer()
 
-def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+def get_current_user(db: Session = Depends(get_db), credentials: HTTPAuthorizationCredentials = Security(security)):
+    token = credentials.credentials
+    if token.startswith("Bearer "):
+        token = token.replace("Bearer ", "", 1)
+    
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
